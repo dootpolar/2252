0fb0bb129dcbf175a67eec78f1812a03a6b4a3ff
dootpolar <dootpolar@gmail.com>
2024-02-26 02:42:19 -0500

    Update ux.js

--- js/ux.js
+++ js/ux.js
@@ -21,45 +21,6 @@ document.onpointerdown = function(t) {
 
 
 
-// Inmediatamente intenta aplicar el modo oscuro basado en la configuración almacenada, si está disponible
-(function() {
-  if (localStorage.getItem("darkmode") === "1") {
-      document.documentElement.classList.add("darkmode");
-      updateThemeColor();
-  }
-})();
-
-// Actualiza los colores del tema basados en la configuración del modo oscuro
-function updateThemeColor() {
-  setTimeout(function() {
-      var themeColor = getComputedStyle(document.documentElement).getPropertyValue('--color-theme');
-      document.querySelector('[name="apple-mobile-web-app-status-bar-style"]').setAttribute('content', themeColor);
-      document.querySelector('[name="msapplication-navbutton-color"]').setAttribute('content', themeColor);
-      document.querySelector('[name="theme-color"]').setAttribute('content', themeColor);
-  }, 1);
-}
-
-// Función para manejar el clic en el botón de modo oscuro
-function darkmode() {
-  var darkModeToggle = document.querySelector('.darkmode-button');
-
-  darkModeToggle.addEventListener('click', function(e) {
-      e.preventDefault();
-      document.documentElement.classList.toggle("darkmode");
-      
-      var isDarkMode = document.documentElement.classList.contains("darkmode");
-      localStorage.setItem("darkmode", isDarkMode ? "1" : "0");
-      
-      updateThemeColor();
-  });
-}
-
-// Asegura que el código se ejecuta después de que el DOM esté completamente cargado
-document.addEventListener('DOMContentLoaded', function() {
-  darkmode(); // Activa la funcionalidad del botón del modo oscuro
-});
-
-
 
 
 
