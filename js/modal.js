5f2269e7597483e5e0af58d245d01d65fac9c922
dootpolar <dootpolar@gmail.com>
2024-02-28 00:54:27 -0500

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,4 +1,4 @@
-
+document.addEventListener('DOMContentLoaded', function() {
 // Contenidos de los modales como literales de plantilla
 const contenidoDetalleVenta = `
 
@@ -220,44 +220,4 @@ window.addEventListener('click', function(event) {
 
 
 
-
-
-
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById('myBtn');
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
-    document.body.classList.add("modal-active"); // Prevent scrolling on body
-    setTimeout(() => { // Timeout for the CSS transition
-      modal.style.opacity = "1";
-      modal.querySelector('.modal-content').style.opacity = "1";
-      modal.querySelector('.modal-content').style.transform = "scale(1)";
-    }, 10);
-}
-
-// When the user clicks on <span> (x), or outside the modal, close it
-function closeModal() {
-    modal.style.opacity = "0";
-    modal.querySelector('.modal-content').style.opacity = "0";
-    modal.querySelector('.modal-content').style.transform = "scale(0.7)";
-    setTimeout(() => { // Timeout for the CSS transition
-      modal.style.display = "none";
-      document.body.classList.remove("modal-active"); // Re-enable scrolling on body
-    }, 300);
-}
-
-span.onclick = closeModal;
-
-window.onclick = function(event) {
-    if (event.target == modal) {
-        closeModal();
-    }
-}
+});
